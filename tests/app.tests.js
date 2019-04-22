762c1bbd946b43042c38b57ff87c4970099f0583
michaelchamu <chamunorwamicahel@gmail.com>
2019-04-22 20:22:52 +0200

    Chai tests
    Added chai test skeleton

---
+++ tests/app.tests.js
@@ -0,0 +1,13 @@
+/// tests for the function to ensure it works correctly regardless of passed parameters
+const assert = require('assert');
+const should = require('chai').should();
+
+describe('Basic tests for sub() function', () => {
+    it('should return error message if a non numeric value exists', () => {});
+    it('should return notification if no arguments are passed', () => {});
+    it('should return result equal to argument if single argument is passed', () => {});
+    it('should return correct result if more than one argument is passed', () => {});
+    it('should accept decimal numbers', () => {});
+    it('should accept a single negative value', () => {});
+    it('should accept multiple negative values', () => {});
+});
