27b08bbf41806520a9012cb6db154f9fac9b7fbd
michaelchamu <chamunorwamicahel@gmail.com>
2019-04-23 15:23:07 +0200

    Refactor default method
    Refactored and imported new styles from config

--- function/defaultvalues.function.js
+++ function/defaultvalues.function.js
@@ -1,23 +1,15 @@
 const cfonts = require('cfonts');
 const { sub } = require('./sub.function');
-const { headingStyle, logStyle } = require('../configs/cfont.configs');
+const { headingStyle, red, yellow, cyan } = require('../configs/cfont.configs');
 cfonts.say('DEFAULT CALCULATION', headingStyle);
 const defaultexecution = () => {
     const promise = new Promise((resolve, reject) => {
         const a = sub(9); // a should be 9
         const b = sub(9, 4); // b should be 5
         const c = sub(14, 2, 6); // c should be 6
-        cfonts.say(`The result of sub(9) is ${a}`, {
-            font: 'console',
-            align: 'center',
-            colors: ['yellow']
-        });
-        cfonts.say(`The result of sub(9, 4) ${b}`, logStyle);
-        cfonts.say(`The result of sub(14, 2, 6) ${c}`, {
-            font: 'console',
-            align: 'center',
-            colors: ['cyan']
-        });
+        cfonts.say(`The result of sub(9) is ${a}`, yellow);
+        cfonts.say(`The result of sub(9, 4) ${b}`, red);
+        cfonts.say(`The result of sub(14, 2, 6) ${c}`, cyan);
         resolve();
     });
     return promise;
