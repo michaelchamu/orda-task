4007adccb328eafd34d0bff1082e5419f92378dc
michaelchamu <chamunorwamicahel@gmail.com>
2019-04-23 11:44:24 +0200

    Refactor main file
    Moved user input function to separate file

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const _ = require('lodash');
-const inquirer = require('inquirer');
 const { sub } = require('./function/sub.function');
-
+const { collectUserInput } = require('./function/inputs.function');
 //default values returned from sub() function
 const a = sub(9); // a should be 9
 console.log(a);
@@ -9,35 +8,9 @@ const b = sub(9, 4); // b should be 5
 console.log(b);
 const c = sub(14, 2, 6); // c should be 6
 console.log(c);
-//start program
-//prompt user for input
-//send inpu to function
-//print result
-//ask user to continue
-
-const collectInputs = async (inputs = []) => {
-    const prompts = [
-        {
-            type: 'input',
-            name: 'numbers',
-            message:
-                'Enter numbers separated by commas. You can enter a single number as well: '
-        },
-        {
-            type: 'confirm',
-            name: 'again',
-            message: 'Would you like to continue and add more number sets? ',
-            default: true
-        }
-    ];
-
-    const { again, ...answers } = await inquirer.prompt(prompts);
-    const newInputs = [...inputs, answers];
-    return again ? collectInputs(newInputs) : newInputs;
-};
 
 const main = async () => {
-    const inputs = await collectInputs();
+    const inputs = await collectUserInput();
 
     for (let i = 0; i < inputs.length; i++) {
         let array = _.split(inputs[i].numbers, ',');
