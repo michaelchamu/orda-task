bb45ac9c32d2c9b127c5f9d056ac26116e5f7f64
michaelchamu <chamunorwamicahel@gmail.com>
2019-04-23 14:55:48 +0200

    Main function print welcome
    Added cfonts in main function to print heading

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const _ = require('lodash');
+const cfonts = require('cfonts');
 const { sub } = require('./function/sub.function');
 const { collectUserInput } = require('./function/inputs.function');
 const { defaultexecution } = require('./function/defaultvalues.function');
-//default values returned from sub() function
 
 const main = async () => {
+    cfonts.say('EXECUTING USER INPUTS FEATURE', {
+        font: 'shade', // define the font face
+        align: 'center', // define text alignment
+        colors: ['greenBright', 'white'], // define all colors
+        background: 'transparent', // define the background color, you can also use `backgroundColor` here as key
+        letterSpacing: 1, // define letter spacing
+        lineHeight: 1, // define the line height
+        space: true, // define if the output text should have empty lines on top and on the bottom
+        maxLength: '0' // define how many character can be on one line
+    });
     const inputs = await collectUserInput();
     for (elements of inputs) {
         let array = _.map(_.split(elements.numbers, ','), value => {
             return parseInt(value);
         });
         let result = sub(...array);
+
         console.log(
             `The result of sub() on ${elements.numbers} is: ${
                 isNaN(result) ? 'Non numeric value detected' : result
