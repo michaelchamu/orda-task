1ea14fe5dc0d957611e20dbba94649f45d4dbf67
michaelchamu <chamunorwamicahel@gmail.com>
2019-04-23 15:07:57 +0200

    Linked headerStyle with heading message

--- app.js
+++ app.js
@@ -3,18 +3,10 @@ const cfonts = require('cfonts');
 const { sub } = require('./function/sub.function');
 const { collectUserInput } = require('./function/inputs.function');
 const { defaultexecution } = require('./function/defaultvalues.function');
+const { headingStyle } = require('./configs/cfont.configs');
 
 const main = async () => {
-    cfonts.say('EXECUTING USER INPUTS FEATURE', {
-        font: 'shade', // define the font face
-        align: 'center', // define text alignment
-        colors: ['greenBright', 'white'], // define all colors
-        background: 'transparent', // define the background color, you can also use `backgroundColor` here as key
-        letterSpacing: 1, // define letter spacing
-        lineHeight: 1, // define the line height
-        space: true, // define if the output text should have empty lines on top and on the bottom
-        maxLength: '0' // define how many character can be on one line
-    });
+    cfonts.say('EXECUTING USER INPUTS FEATURE', headingStyle);
     const inputs = await collectUserInput();
     for (elements of inputs) {
         let array = _.map(_.split(elements.numbers, ','), value => {
