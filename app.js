ca9d996b62f6900b58ea06b34cefc4ec7f54e937
michaelchamu <chamunorwamicahel@gmail.com>
2019-04-23 13:59:55 +0200

    Moved default values to functions folder
    Moved the values calculated by default to functions folder

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const _ = require('lodash');
 const { sub } = require('./function/sub.function');
 const { collectUserInput } = require('./function/inputs.function');
+const { defaultexecution } = require('./function/defaultvalues.function');
 //default values returned from sub() function
-const a = sub(9); // a should be 9
-console.log(`The result of sub(9) is ${a}`);
-const b = sub(9, 4); // b should be 5
-console.log(`The result of sub(9, 4) ${b}`);
-const c = sub(14, 2, 6); // c should be 6
-console.log(`The result of sub(14, 2, 6) ${c}`);
+
 const main = async () => {
     const inputs = await collectUserInput();
     for (elements of inputs) {
@@ -22,5 +18,7 @@ const main = async () => {
         );
     }
 };
-
-main();
+//execute the default values then the main function that asks for user input
+defaultexecution().then(() => {
+    main();
+});
