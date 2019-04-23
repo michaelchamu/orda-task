c66a7d7b55f1d06671a934114110ab7864c262ad
michaelchamu <chamunorwamicahel@gmail.com>
2019-04-23 14:49:41 +0200

    Heading for default operation
    Added stylised heading for default operation

--- function/defaultvalues.function.js
+++ function/defaultvalues.function.js
@@ -1,14 +1,35 @@
 const cfonts = require('cfonts');
 const { sub } = require('./sub.function');
-
+cfonts.say('EXECUTING DEFAULT CALCULATION', {
+    font: 'shade', // define the font face
+    align: 'center', // define text alignment
+    colors: ['greenBright', 'white'], // define all colors
+    background: 'transparent', // define the background color, you can also use `backgroundColor` here as key
+    letterSpacing: 1, // define letter spacing
+    lineHeight: 1, // define the line height
+    space: true, // define if the output text should have empty lines on top and on the bottom
+    maxLength: '0' // define how many character can be on one line
+});
 const defaultexecution = () => {
     const promise = new Promise((resolve, reject) => {
         const a = sub(9); // a should be 9
-        console.log(`The result of sub(9) is ${a}`);
         const b = sub(9, 4); // b should be 5
-        console.log(`The result of sub(9, 4) ${b}`);
         const c = sub(14, 2, 6); // c should be 6
-        console.log(`The result of sub(14, 2, 6) ${c}`);
+        cfonts.say(`The result of sub(9) is ${a}`, {
+            font: 'console',
+            align: 'center',
+            colors: ['yellow']
+        });
+        cfonts.say(`The result of sub(9, 4) ${b}`, {
+            font: 'console',
+            align: 'center',
+            colors: ['red']
+        });
+        cfonts.say(`The result of sub(14, 2, 6) ${c}`, {
+            font: 'console',
+            align: 'center',
+            colors: ['cyan']
+        });
         resolve();
     });
     return promise;
