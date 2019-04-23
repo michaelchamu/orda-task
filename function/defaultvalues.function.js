d557cf0734bd52bc692c4b73e64943e79552f914
michaelchamu <chamunorwamicahel@gmail.com>
2019-04-23 15:17:42 +0200

    Added style objects from cfont config

--- function/defaultvalues.function.js
+++ function/defaultvalues.function.js
@@ -1,15 +1,7 @@
 const cfonts = require('cfonts');
 const { sub } = require('./sub.function');
-cfonts.say('DEFAULT CALCULATION', {
-    font: 'shade', // define the font face
-    align: 'center', // define text alignment
-    colors: ['greenBright', 'white'], // define all colors
-    background: 'transparent', // define the background color, you can also use `backgroundColor` here as key
-    letterSpacing: 1, // define letter spacing
-    lineHeight: 1, // define the line height
-    space: true, // define if the output text should have empty lines on top and on the bottom
-    maxLength: '0' // define how many character can be on one line
-});
+const { headingStyle, logStyle } = require('../configs/cfont.configs');
+cfonts.say('DEFAULT CALCULATION', headingStyle);
 const defaultexecution = () => {
     const promise = new Promise((resolve, reject) => {
         const a = sub(9); // a should be 9
@@ -20,11 +12,7 @@ const defaultexecution = () => {
             align: 'center',
             colors: ['yellow']
         });
-        cfonts.say(`The result of sub(9, 4) ${b}`, {
-            font: 'console',
-            align: 'center',
-            colors: ['red']
-        });
+        cfonts.say(`The result of sub(9, 4) ${b}`, logStyle);
         cfonts.say(`The result of sub(14, 2, 6) ${c}`, {
             font: 'console',
             align: 'center',
