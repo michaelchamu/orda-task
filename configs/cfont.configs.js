6015532b774b270787bfde7c58ff339361bf7f09
michaelchamu <chamunorwamicahel@gmail.com>
2019-04-23 15:06:55 +0200

    Added headerstyle settings to config file

--- configs/cfont.configs.js
+++ configs/cfont.configs.js
@@ -1,4 +1,13 @@
-const headingStyle = {};
+const headingStyle = {
+    font: 'shade', // define the font face
+    align: 'center', // define text alignment
+    colors: ['greenBright', 'white'], // define all colors
+    background: 'transparent', // define the background color, you can also use `backgroundColor` here as key
+    letterSpacing: 1, // define letter spacing
+    lineHeight: 1, // define the line height
+    space: true, // define if the output text should have empty lines on top and on the bottom
+    maxLength: '0' // define how many character can be on one line
+};
 const logStyle = {};
 
 module.exports = { headingStyle, logStyle };
